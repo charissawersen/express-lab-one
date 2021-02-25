09438bae5dd2fedcb8549c3a02610d516c7d0878
Charissa Wersen <charissa.wersen@gmail.com>
2021-02-24 21:54:00 -0500

    initial commit

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const cart = require('./cart');
+const express = require("express");
 
 const app = express();
-app.use(express.json());
-
 const port = 3000;
 
 const cors = require("cors");
-
 app.use(cors());
+
 app.use(express.static(__dirname + "/public"));
 
-app.use("/cart", cart);
+app.use(express.json());
+
+const cart = require("./cart");
+app.use("/cart-items", cart);
 
 app.listen(port, () => console.log(`Listening on port:  ${port}.`));
-console.log("http://localhost:" + port)
+console.log("http://localhost:" + port);
--- cart.js
+++ cart.js
@@ -1,77 +1,87 @@
-const { request, response } = require("express");
+// const { request, response } = require("express");
 const express = require("express");
 const cart = express.Router();
-const pool = require('./pg-connection-pool');
 
-const cartList = [
-  { id: 1, item: "laptop", price: 1500, quantity: 5 },
-  { id: 2, item: "guitar", price: 200, quantity: 10 },
-  { id: 3, item: "textbook", price: 100, quantity: 50 },
-  { id: 4, item: "headphones", price: 75, quantity: 15 },
-  { id: 5, item: "charger", price: 12, quantity: 35 },
-  { id: 6, item: "fancy folder", price: 3, quantity: 150 },
-  { id: 7, item: "altoid", price: 3, quantity: 200 },
-  { id: 8, item: "backpack", price: 50, quantity: 15 },
-];
+const pool = require("./pg-connection-pool.js");
 
 cart.get("/", (req, res) => {
-  // filteredcart = cartlist
-  const price = req.query.price;
+  let maxPrice = req.query.maxPrice;
+  if (maxPrice) {
+    pool
+      .query("SELECT * FROM shopping_cart WHERE price <=$1", [maxPrice])
+      .then((results) => {
+        res.status(200).json(results.rows);
+      });
+  }
   const prefix = req.query.prefix;
-  const pageSize = req.query.pageSize;
-
-  if (price) {
-    const newCart = cartList.filter((maxPrice) => {
-      return maxPrice.price <= price;
-    });
-    res.json(newCart);
-  }
-
   if (prefix) {
-    const itemList = cartList.filter((items) => {
-      return items.item.toLowerCase().startsWith(prefix.toLowerCase());
-    })
-    //startsWith
-    res.json(itemList)
-  }
+    pool
+      .query("SELECT * FROM shopping_cart WHERE product LIKE $1", [
+        prefix + "%",
+      ])
+      .then((results) => {
 
-  if (pageSize) {
-    const newList = cartList.slice(0, parseInt(req.query.pageSize));
-    res.json(newList);
+        res.status(200).json(results.rows);
+      });
   }
-  res.status(200).json(cartList);
+  const pageSize = parseInt(req.query.pageSize);
+  if (pageSize) {
+    pool
+      .query("SELECT * FROM shopping_cart LIMIT $1", [pageSize])
+      .then((results) => {
+        res.status(200).json(results.rows);
+      });
+  } else {
+    pool.query("SELECT * FROM shopping_cart").then((results) => {
+      res.status(200).json(results.rows);
+    });
+  }
 });
 
-cart.get("/:id", (request, response) => {
-  var cartItem = cartList.find(
-    (cartItem) => cartItem.id === parseInt(request.params.id)
-  );
-  if (!cartItem) {
-    response.status(404).send("ID Not Found");
-  }
-  response.status(200).json(cartItem);
+cart.get("/:id", (req, res) => {
+  let id = parseInt(req.params.id);
+  pool
+    .query("SELECT * FROM shopping_cart WHERE id = $1", [id])
+    .then((results) => {
+      // if (!id) {
+      //   res.status(404).send("ID Not Found!");
+      // } else {
+      res.status(200).json(results.rows);
+      // }
+    });
 });
 
 cart.post("/", (req, res) => {
   let item = req.body;
-  item.id = cartList.length + 1;
-  cartList.push(req.body);
-  res.status(201).json(cartList);
+  let price = parseInt(req.body.price);
+  pool
+    .query(
+      `INSERT INTO shopping_cart(product, price, quantity) VAlUES ($1, $2, $3)`,
+      [item.product, price, item.quantity]
+    )
+    .then((results) => {
+      res.status(201).json(item);
+    });
 });
 
 cart.put("/:id", (req, res) => {
-  const index = cartList.findIndex((item) => {
-    return item.id === parseInt(req.params.id);
-  })
-  cartList.splice(index, 1, req.body);
-  res.status(200).json(cartList);
+  let item = req.body;
+  let id = parseInt(req.params.id);
+  pool
+    .query(
+      `UPDATE shopping_cart SET product=$1, price=$2, quantity=$3 WHERE id=$4`,
+      [item.product, item.price, item.quantity, id]
+    )
+    .then((results) => {
+      res.status(200).json(item);
+    });
 });
 
 cart.delete("/:id", (req, res) => {
-  const deleteIndex = cartList.findIndex((item) => {
-    return item.id === parseInt(req.params.id);
-  });
-  cartList.splice(deleteIndex, 1);
-  res.status(204).json(cartList);
-});
+  let id = parseInt(req.params.id);
+  pool.query(`DELETE FROM shopping_cart WHERE id=$1`, [id]).then((results) => {
+    res.status(204).json(results.rows);
+  })
+})
+
 module.exports = cart;
